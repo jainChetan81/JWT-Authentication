7d5a277fb4212cb14565471b5824454d01542c12
jainChetan81 <jain.cj.chetan@gmail.com>
2022-03-10 07:32:30 +0530

    added basic hashing

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,13 +2,16 @@ import { Response, Request } from "express";
 import users from "../db";
 import { userSignupValidator } from "../helpers";
 import { User } from "../types";
+import bcrypt from "bcrypt";
 
 const authRouter = require("express").Router();
-authRouter.post("/signup", userSignupValidator, (req: Request, res: Response) => {
+authRouter.post("/signup", userSignupValidator, async (req: Request, res: Response) => {
 	const { email, password } = req.body;
 	console.log(email, password);
 	const user: User | undefined = users.find((user) => user.email === email);
-	if (user) res.status(409).send("User already exists");
+	if (user) return res.status(409).send("User already exists");
+	let hashedPassword = await bcrypt.hash(password, 10);
+	console.log(hashedPassword);
 	res.send("Well done auth!");
 });
 
